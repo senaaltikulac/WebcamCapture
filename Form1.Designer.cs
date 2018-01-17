fe347bdeb5de8d213e14c68d32119c7dcd0dd022
senaaltikulac <senaaltikulac@gmail.com>
2018-01-18 01:26:05 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WebcamCapture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnContinue = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(568, 485);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnStart.Location = new System.Drawing.Point(207, 534);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(188, 33);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Take a Picture";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStop.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnStop.Location = new System.Drawing.Point(12, 534);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(173, 33);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnContinue
+            // 
+            this.btnContinue.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnContinue.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnContinue.Location = new System.Drawing.Point(414, 534);
+            this.btnContinue.Name = "btnContinue";
+            this.btnContinue.Size = new System.Drawing.Size(166, 33);
+            this.btnContinue.TabIndex = 4;
+            this.btnContinue.Text = "Start";
+            this.btnContinue.UseVisualStyleBackColor = true;
+            this.btnContinue.Click += new System.EventHandler(this.btnContinue_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 507);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(568, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(602, 574);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnContinue);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnContinue;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace WebcamCapture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        } 
+        private FilterInfoCollection webcam;  // bilgisayar içinde bulunan kameraları tutuyor
+        private VideoCaptureDevice cam;  // bizim seçeceğimiz aygıtı tutuyor.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo item in webcam) // var olan aygıtları combobox a ekliyoruz.
+            {
+                comboBox1.Items.Add(item.Name);
+
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog swf = new SaveFileDialog(); // çekilen resmi kaydediyoruz.
+            swf.Filter = "(*jpg | *.jpg| *.bmp | *.bmp";
+
+            DialogResult dialog=swf.ShowDialog();
+            if(dialog ==DialogResult.OK)
+            {
+                pictureBox1.Image.Save(swf.FileName);
+            }
+        }
+        void cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();//kameradan alınan görüntüyü picturebox a atar
+            pictureBox1.Image = bmp; // kameradaki görüntüyü ekler
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (cam.IsRunning)
+            {
+                cam.Stop();
+                pictureBox1.Image = null; // picturebox ı boşaltır
+            }
+        }
+
+        private void btnContinue_Click(object sender, EventArgs e)
+        {
+            cam = new VideoCaptureDevice(webcam[comboBox1.SelectedIndex].MonikerString); //cam e comboboxtan seçilmiş olanı kameraya atar
+            cam.NewFrame += new NewFrameEventHandler(cam_NewFrame); // 
+            cam.Start(); // kamerayı başlatır.
+        }
+    }
+}
